1926ef2d48029d8cc1d5bf4ea9e22c946e818ca5
guldamas <lapitskiy.vadim@gmail.com>
2021-11-05 16:41:48 +0200

    commit test_main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
 
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self, url=None):
         self.browser.get(url or self.url)
 
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
-if __name__ == '__main__':
-    page = BasePage(webdriver.Chrome(), 'https://google.com')
-    page.open()
+
+# if __name__ == '__main__':
+#     page = BasePage(webdriver.Chrome(), 'https://google.com')
+#     page.open()
