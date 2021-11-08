0eb0c11d1700a40277f0d944ec8ee8e0f9102c24
guldamas <lapitskiy.vadim@gmail.com>
2021-11-08 15:51:18 +0200

    commit add current_url.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,13 +3,13 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser: webdriver.WebKitGTK, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self, url=None):
-        self.browser.get(url or self.url)
+    def open(self):
+        self.browser.get(self.url)
 
     def is_element_present(self, how, what):
         try:
@@ -18,7 +18,11 @@ class BasePage:
             return False
         return True
 
+    def get_current_url(self):
+        return self.browser.current_url
 
-# if __name__ == '__main__':
-#     page = BasePage(webdriver.Chrome(), 'https://google.com')
-#     page.open()
+
+if __name__ == '__main__':
+    page = BasePage(webdriver.Chrome(), 'https://google.com')
+    page.open()
+    print(page.get_current_url())
